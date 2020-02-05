42557f3cc87c12280228a35f1cc618bd17c8b270
imdodcc13 <imdodcc12@gmail.com>
2020-02-05 16:51:17 -0300

    act

--- js/statitics.js
+++ js/statitics.js
@@ -4,7 +4,12 @@ let stats = {
     "totalDem": 0,
     "votedWDem": 0,
     "totalInd": 0,
-    "votedWInd": 0, 
+    "votedWInd": 0,
+    "totalMembers": 0,
+    "leastEngaged": [],
+    "mostEngaged": [],
+    "leastLoyal": [], 
+    "mostLoyal": [],
 }
 
 let members = data.results[0].members;
@@ -27,12 +32,67 @@ members.forEach(e => {
     }
 })
 
+stats.totalMembers = stats.totalRep + stats.totalDem + stats.totalInd;
 stats.votedWRep = votesRep/stats.totalRep;
 stats.votedWDem = votesDem/stats.totalDem;
 stats.votedWInd = votesInd/stats.totalInd;
 
-let table = document.querySelector("#table-total");
-let tbody = document.querySelector("tbody");
+let tenPct = Math.round(stats.totalMembers*0.1);
+
+let tbody = document.querySelector("#table-total>tbody");
+let row = tbody.insertRow(-1);
+row.innerHTML = `<td>Republican</td><td>${stats.totalRep}</td><td>${(stats.votedWRep).toFixed(2)}%`;
+row = tbody.insertRow(-1);
+row.innerHTML = `<td>Democrat</td><td>${stats.totalDem}</td><td>${(stats.votedWDem).toFixed(2)}%`;
+row = tbody.insertRow(-1);
+row.innerHTML = `<td>Independant</td><td>${stats.totalInd}</td><td>${(stats.votedWInd).toFixed(2)}%`;
+row = tbody.insertRow(-1);
+row.innerHTML = `<td>Total</td><td>${stats.totalMembers}</td><td></td>`;
 
 
+function compareVotes(memberA, memberB){
+    return memberA.missed_votes_pct - memberB.missed_votes_pct;
+}
+
+let datosOrd = members.sort(compareVotes);
+
+function addRestTop(index,array){
+    let valueAct = datosOrd[index].missed_votes_pct;
+    for(let i = index + 1; i < datosOrd.length; i++){
+        if(datosOrd[i].missed_votes_pct == valueAct){
+            array.push(datosOrd[i]);
+        }
+    }
+}
+
+function addRestBot(index,array){
+    let valueAct = datosOrd[index].missed_votes_pct;
+
+}
+
+for(let i = 0; i < tenPct; i++){
+    stats.mostEngaged.push(datosOrd[i]);
+    if(i == tenPct-1){
+        addRestBot(i,stats.mostEngaged);
+    }
+}
+
+for(let i = datosOrd.length -1; i >= (datosOrd.length - tenPct); i--){
+    stats.leastEngaged.push(datosOrd[i]);
+    if(i == datosOrd.length - tenPct){}
+}
+
+tbody = document.querySelector("#table-least>tbody");
+stats.leastEngaged.forEach(member => {
+    let row = tbody.insertRow(-1);
+    row.innerHTML = `<td>${member.last_name}, ${member.first_name} ${member.middle_name?member.middle_name:""}</td>`
+    +`<td>${member.missed_votes}</td><td>${member.missed_votes_pct.toFixed(2)}%</td>`;
+})
+
+tbody = document.querySelector("#table-most>tbody");
+stats.mostEngaged.forEach(member => {
+    let row = tbody.insertRow(-1);
+    row.innerHTML = `<td>${member.last_name}, ${member.first_name} ${member.middle_name?member.middle_name:""}</td>`
+    +`<td>${member.missed_votes}</td><td>${member.missed_votes_pct.toFixed(2)}%</td>`;
+})
 
